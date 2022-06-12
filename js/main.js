a214d9a1ce4c767b3b9c666b400a9217cbc9c75b
Régis Universo <regisuniverso@gmail.com>
2022-06-12 20:11:08 -0300

    Commit da Aula 1

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+
+var nome = "Régis Universo";
+var idade1 = 55;
+var idade2 = 24;
+var idade3 = "12";
+var idade4 = "34";
+var frase = "Eu, " + nome + ", sou a mosca que pousou em sua sopa";
+
+         // CONCATENANDO STRINGS
+//alert("BEM VINDO " + nome + "!!!");
+//alert(nome + " tem " + idade1 + " anos!!!");
+//alert(idade3 + idade4);
+
+console.log(nome);        
+
+          //  REPLACE = SUBSTITUINDO TERMOS
+//alert(frase);
+//alert(frase.replace("mosca", "libélula"));
+console.log(frase);
+console.log(frase.replace("mosca", "libélula"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+
+          // NUMBERS
+var n1 = 5;
+var n2 = 10;
+
+//alert(n1 + n2);
+console.log(n1 + n2);
+
